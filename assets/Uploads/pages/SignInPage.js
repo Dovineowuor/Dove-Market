97327d4ec12533bed4a5d30a2b8b2ddc386cff8b
dovineowuor <owuordove@gmail.com>
2022-12-31 09:41:43 +0300

    Update && git push

---
+++ assets/Uploads/pages/SignInPage.js
@@ -0,0 +1,93 @@
+import "./SignInPage.css";
+
+const SignInPage = () => {
+  return (
+    <div className="sign-in-page">
+      <div className="frame6">
+        <div className="frame7">
+          <div className="rectangle206" />
+          <div className="frame8">
+            <div className="rectangle207" />
+            <div className="frame9">
+              <div className="rectangle208" />
+              <div className="rectangle209" />
+              <div className="frame10">
+                <button className="rectangle210" />
+                <div className="frame11">
+                  <div className="rectangle211" />
+                  <div className="rectangle212" />
+                  <div className="continue">Continue</div>
+                </div>
+              </div>
+              <pre className="by-continuing-you-agree-to-do">
+                By continuing, you agree to Dove Market's
+              </pre>
+              <div className="frame12">
+                <a className="conditions-of-use2" href="/condition-of-use">
+                  Conditions of Use
+                </a>
+                <a className="privacy-notice2" href="/privace-policy">
+                  Privacy Notice
+                </a>
+                <pre className="and">and</pre>
+                <pre className="pre">.</pre>
+              </div>
+              <div className="rectangle213" />
+              <div className="rectangle214" />
+            </div>
+            <div className="sign-in2">Sign in</div>
+            <div className="frame13">
+              <a className="need-help" href="/Customer-support-page">
+                Need help?
+              </a>
+              <img className="image-icon235" alt="" src="../image236@2x.png" />
+            </div>
+            <div className="frame14">
+              <input
+                className="rectangle215"
+                type="text"
+                placeholder="E-mail / Phone Number"
+              />
+              <strong className="email-or-mobile-phone-number">
+                Email or mobile phone number
+              </strong>
+            </div>
+          </div>
+        </div>
+        <a className="frame15">
+          <div className="rectangle216" />
+          <div className="new-to-dove-market">New to Dove Market?</div>
+        </a>
+        <div className="rectangle217" />
+        <div className="rectangle218" />
+        <div className="rectangle219" />
+        <div className="rectangle220" />
+        <button className="frame16">
+          <div className="rectangle221" />
+          <div className="frame17">
+            <div className="rectangle222" />
+            <div className="create-your-dove-market-accoun">
+              Create your Dove Market account
+            </div>
+          </div>
+        </button>
+      </div>
+      <div className="frame18">
+        <div className="rectangle223" />
+        <div className="frame19">
+          <a className="conditions-of-use3" href="/Conditions-of-use">
+            Conditions of Use
+          </a>
+          <a className="privacy-notice3" href="/privacy">
+            Privacy Notice
+          </a>
+          <a className="help">Help</a>
+        </div>
+        <div className="dove-market-inc2">© 2022, Dove Market, Inc.</div>
+      </div>
+      <a className="dove-marketlogos-12x-1" href="/index.html" />
+    </div>
+  );
+};
+
+export default SignInPage;
